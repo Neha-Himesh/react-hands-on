34531bea7888c4c7504bf7b8993baff2714c8587
Neha-Himesh <neha.himesh@spike.sh>
2025-05-19 23:52:21 +0530

    Added pure component demo code

--- src/App.js
+++ src/App.js
@@ -19,13 +19,14 @@ import Form from './components/Form';
 import LifecycleA from './components/LifecycleA';
 import FragmentDemo from './components/FragmentDemo';
 import Table from './components/Table';
+import ParentComp from './components/ParentComp';
 
 class App extends Component {
 
   render(){
     return (
       <div className="App">
-        <Table />
+        <ParentComp />
       </div>
     );
   }
---
+++ src/components/ParentComp.js
@@ -0,0 +1,32 @@
+import React, { Component, PureComponent } from 'react'
+import RegComp from './RegComp'
+import PureComp from './PureComp'
+
+class ParentComp extends PureComponent {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         name: 'Neha'
+      }
+    }
+    componentDidMount(){
+        setInterval(()=>{
+            this.setState({
+                name: 'Neha'
+            })
+        }, 2000)
+    }
+  render() {
+    console.log("*** Parent Component render ***")
+    return (
+        <div>
+            Parent Component
+            <RegComp name={this.state.name} />
+            <PureComp name={this.state.name} />
+        </div>
+    )
+  }
+}
+
+export default ParentComp
