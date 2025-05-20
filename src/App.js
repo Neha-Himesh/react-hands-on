380bcdcf39edacdd26f47bf023337e95aa2391e7
Neha-Himesh <neha.himesh@spike.sh>
2025-05-20 23:38:33 +0530

    Added code for ErrorBounding, Fragments, Component, Memo, ParentComponent, Higher Order Components

--- src/App.js
+++ src/App.js
@@ -1,33 +1,22 @@
 import logo from './logo.svg';
 import React, {Component} from 'react'
 import './App.css';
-import Greet from './components/Greet'
-import Welcome from './components/Welcome'
-import Hello from './components/Hello'
-import Message from './components/Message';
-import Counter from './components/Counter';
-import FunctionClick from './components/FunctionClick';
-import ClassClick from './components/ClassClick';
-import EventBind from './components/EventBind';
-import ParentComponent from './components/ParentComponent';
-import NameList from './components/NameList';
-import Stylesheet from './components/Stylesheet';
-import Inline from './components/Inline';
-import './appStyles.css';
-import styles from './appStyles.module.css';
-import Form from './components/Form';
-import LifecycleA from './components/LifecycleA';
-import FragmentDemo from './components/FragmentDemo';
-import Table from './components/Table';
-import ParentComp from './components/ParentComp';
+import FocusInput from './components/FocusInput';
+import FRParentInput from './components/FRParentInput';
+import Hero from './Hero';
+import ErrorBoundary from './components/ErrorBoundary';
+import ClickCounter from './components/ClickCounter';
+import HoverCounter from './components/HoverCounter';
+
 
 class App extends Component {
 
   render(){
     return (
-      <div className="App">
-        <ParentComp />
-      </div>
+    	<div className="App">
+			<ClickCounter name='Neha'/>
+			<HoverCounter/>
+     	</div>
     );
   }
   
--- src/components/ParentComp.js
+++ src/components/ParentComp.js
@@ -1,8 +1,9 @@
 import React, { Component, PureComponent } from 'react'
 import RegComp from './RegComp'
 import PureComp from './PureComp'
+import MemoComp from './MemoComp'
 
-class ParentComp extends PureComponent {
+class ParentComp extends Component {
     constructor(props) {
       super(props)
     
@@ -18,12 +19,13 @@ class ParentComp extends PureComponent {
         }, 2000)
     }
   render() {
-    console.log("*** Parent Component render ***")
+    // console.log("*** Parent Component render ***")
     return (
         <div>
             Parent Component
-            <RegComp name={this.state.name} />
-            <PureComp name={this.state.name} />
+            {/* <RegComp name={this.state.name} />
+            <PureComp name={this.state.name} /> */}
+            <MemoComp name={this.state.name}/>
         </div>
     )
   }
