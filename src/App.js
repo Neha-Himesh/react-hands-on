bd288390bedae98fc3d4f7185d63e99075bbd89e
Neha-Himesh <neha.himesh@spike.sh>
2025-05-23 00:38:25 +0530

    Added code for render props react

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ import ErrorBoundary from './components/ErrorBoundary';
 import ClickCounter from './components/ClickCounter';
 import HoverCounter from './components/HoverCounter';
 import PortalDemo from './components/PortalDemo';
+import ClickCounterTwo from './components/ClickCounterTwo';
+import HoverCounterTwo from './components/HoverCounterTwo';
+import User from './components/User';
+import CounterRenderProps from './components/CounterRenderProps';
 
 
 class App extends Component {
@@ -15,7 +19,8 @@ class App extends Component {
   render(){
     return (
     	<div className="App">
-        <PortalDemo />
+        <CounterRenderProps render={(count, incrementCount) => (<ClickCounterTwo count={count} incrementCount={incrementCount} /> )} />
+        <CounterRenderProps render={(count, incrementCount) => (<HoverCounterTwo count={count} incrementCount={incrementCount} /> )} />
      	</div>
     );
   }
