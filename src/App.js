66b396a10163b2df012950ff80d6d536ecc6745b
Neha-Himesh <neha.himesh@spike.sh>
2025-05-27 00:15:49 +0530

    Added code related to useEffect of react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import logo from './logo.svg';
+
 import React, {Component} from 'react'
-import './App.css';
-import FocusInput from './components/FocusInput';
-import FRParentInput from './components/FRParentInput';
-import Hero from './Hero';
-import ErrorBoundary from './components/ErrorBoundary';
-import ClickCounter from './components/ClickCounter';
-import HoverCounter from './components/HoverCounter';
-import PortalDemo from './components/PortalDemo';
-import ClickCounterTwo from './components/ClickCounterTwo';
-import HoverCounterTwo from './components/HoverCounterTwo';
-import User from './components/User';
-import CounterRenderProps from './components/CounterRenderProps';
-import ComponentC from './components/ComponentC';
-import { UserProvider } from './components/userContext';
-import HookCounter from './components/HookCounter';
-import HookCounterTwo from './components/HookCounterTwo';
-import HookCounterThree from './components/HookCounterThree';
-import HookCounterFour from './components/HookCounterFour';
+import IntervalHookCounter from './components/IntervalHookCounter';
+import DataFetching from './components/DataFetching';
 
 
 class App extends Component {
@@ -25,7 +9,7 @@ class App extends Component {
   render(){
     return (
     	<div className="App">
-        <HookCounterFour/>
+        <DataFetching/>
      	</div>
     );
   }
